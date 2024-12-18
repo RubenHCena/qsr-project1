f156a79e583807948b5f810d7703c15a42970b9f
RubenHCena <rubencena@cenas.net>
2024-12-18 10:39:24 +0100

    added categories

--- src/pages/ShopList.vue
+++ src/pages/ShopList.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page class="flex flex-top-left ">
-    <div class="q-pa-md">
+    <div class="q-pa-md row">
 
       <q-form
         @submit.prevent="saveItem"
@@ -15,7 +15,41 @@
           :rules="[ val => val && val.length > 1 || 'Please type something']"
         />
 
-        <q-toggle v-model="newItemHighPrio" label="High Priority" />
+      <q-toggle v-model="newItemHighPrio" label="High Priority" />
+
+      <div>
+        <q-btn-dropdown color="primary" label="Category">
+          <q-list>
+            <q-item clickable v-close-popup @click="foodClick">
+              <q-item-section>
+                <q-item-label>
+                  <q-avatar icon="fastfood" />
+                  Food Or Drinks
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-item clickable v-close-popup @click="clothesClick">
+              <q-item-section>
+                <q-item-label>
+                  <q-avatar icon="checkroom" />
+                  Clothes
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-item clickable v-close-popup @click="otherClick">
+              <q-item-section>
+                <q-item-label>
+                  <q-avatar icon="more_horiz"/>
+                  Other
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-btn-dropdown>
+      </div>
+      
 
         <div>
           <q-btn label="Submit" type="submit" color="primary"/>
@@ -31,13 +65,16 @@
           :class="[{prioStyle: item.highPrio}]"
           >
           <q-slide-item>
-            <q-avatar>
-            </q-avatar>
+            <q-avatar :icon="item.icon" />
               {{ item.id }}
               {{ item.text }} 
             <template v-slot:left>
+              <div class="row">
               <q-icon name="done" />
               {{ item.text }}
+              <q-space/>
+              <q-icon name="close" />
+            </div>
             </template>
           </q-slide-item>
           </q-slide-item>
@@ -55,7 +92,10 @@
 
   const newItem = ref("")
   const newItemHighPrio = ref(false)
+  const newItemIcon = ref("")
   const $q = useQuasar()
+
+
 
   let items = ref([
                       {
@@ -63,6 +103,7 @@
                         text: "hatter",
                         //purchased: false,
                         highPrio: false,
+                        icon: "fastfood",
                       },
                       {
                         id: 2,
@@ -72,12 +113,24 @@
                       }
   ])
   const saveItem = ()=>{
-    items.value.push({id: items.value.length + 1, text: newItem.value, purchased: false, highPrio: newItemHighPrio.value})                     
+    items.value.push({id: items.value.length + 1, text: newItem.value, purchased: false, highPrio: newItemHighPrio.value, icon: newItemIcon.value})                     
     newItem.value = ""
+    newItemIcon.value = ""
   }
 
-  function deleteItem(index) { console.log("kfesfsofkse")
+  function deleteItem(index) { 
+    console.log("kfesfsofkse");
     $q.this.items.value = this.items.value.splice(index, 1); 
+  }
+
+  function foodClick() {
+    newItemIcon.value = "fastfood"
+  }
+  function clothesClick() {
+    newItemIcon.value = "checkroom"
+  }
+  function otherClick() {
+    newItemIcon.value = "more_horiz"
   }
 
 
